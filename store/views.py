aff122433f41693dfd4166e1a5ac5ac160730786
Kerry Tolhurst <k_tolhurst@hotmail.co.uk>
2023-12-10 01:20:08 +0000

    Update cart view to create order instance with order items and render to cart template

--- store/views.py
+++ store/views.py
@@ -17,7 +17,27 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    '''
+    Fetches or creates a single :model: `Order` instance for the current customer dynamically
+    by logged in status and sets it's items before rendering it 
+
+    **Context**
+
+    ``items``
+        All items with this order ID to be attached to the order
+
+    :template:`store/cart.html`
+    '''
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
+    context = {
+        'items' : items
+    }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
