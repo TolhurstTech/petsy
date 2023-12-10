4ecaa2a20539be08ce3fbb69db605d7258e3b15b
Kerry Tolhurst <k_tolhurst@hotmail.co.uk>
2023-12-10 02:57:30 +0000

    Update checkout view + template to render customers order data

--- store/views.py
+++ store/views.py
@@ -28,7 +28,7 @@ def cart(request):
 
     ``order``
         The users current order/open cart object
-        
+
     :template:`store/cart.html`
     '''
     if request.user.is_authenticated:
@@ -37,6 +37,7 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
     context = {
         'items' : items,
@@ -45,5 +46,16 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {
+        'items' : items,
+        'order' : order
+    }
     return render(request, 'store/checkout.html', context)
