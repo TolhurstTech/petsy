1a55806fb64ccfa5a4801f25eade0a8889104f79
Kerry Tolhurst <k_tolhurst@hotmail.co.uk>
2023-12-12 23:07:32 +0000

    Send cart items in the context for product_detail view so basket total works on the page

--- store/views.py
+++ store/views.py
@@ -50,10 +50,22 @@ def product_detail(request, slug):
     queryset = Product.objects.filter(draft=1)
     product = get_object_or_404(queryset, slug=slug)
 
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
     return render(
         request,
         "store/product_detail.html",
-        {"product": product},
+        {"product": product,
+         "cartItems": cartItems
+        },
     )
 
 def cart(request):
