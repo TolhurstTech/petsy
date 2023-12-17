462f19b1d2883aa0af43bc5de8749504a2364814
Kerry Tolhurst <k_tolhurst@hotmail.co.uk>
2023-12-17 22:43:20 +0000

    views.py linted

--- store/views.py
+++ store/views.py
@@ -4,39 +4,43 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect, JsonResponse
 import json
 import datetime
-from .models import Customer, Product, Order, OrderItem, ShippingAddress, Review
+from .models import Customer, Product, Order, OrderItem, ShippingAddress, Review  # noqa
 from .forms import ReviewForm
 
 
 # Create your views here.
 
-class ProductList(generic.ListView): 
+class ProductList(generic.ListView):
     '''
-    Displays all products from :model:`store.Product` using djangos list view with pagination.
+    Displays all products from :model:`store.Product`
+    using djangos list view with pagination.
 
     **Context**
 
     ``items``
-        The items for any open carts to calculate cart totals in the navbar in this view from :model:`OrderItem`
+        The items for any open carts to calculate cart totals
+        in the navbar in this view from :model:`OrderItem`
 
     ``order``
         The users current order/open cart object from :model:`store.Order`
 
     ``cartItems``
-        Total number of cart items for the basket total from :model:`store.Order`
+        Total number of cart items for the
+        asket total from :model:`store.Order`
 
     ``product``
         Sent through by default to display each product :model:`store.Product`
 
     :template:`store/store.html`
-    '''   
+    '''
     model = Product
     queryset = Product.objects.filter(draft=1)
     template_name = "store/store.html"
     paginate_by = 6
 
-    # Override class context method to pass cart total as cartItems to the view template
-    def get_context_data(self,**kwargs):
+    # Override class context method to pass cart
+    # total as cartItems to the view template
+    def get_context_data(self, **kwargs):
         # Check if guest cart
         if self.request.user.is_authenticated:
 
@@ -44,56 +48,72 @@ class ProductList(generic.ListView):
             try:
                 customer = customer = self.request.user.customer
             except AttributeError:
-                customer, created = Customer.objects.get_or_create(user=self.request.user, name=self.request.user.first_name + " " + self.request.user.last_name, email=self.request.user.email)
-            
-            order, created = Order.objects.get_or_create(customer=customer, complete=False)
-            context = super(ProductList,self).get_context_data(**kwargs)
+                customer, created = Customer.objects.get_or_create(
+                    user=self.request.user,
+                    name=self.request.user.first_name,
+                    email=self.request.user.email
+                )
+
+            order, created = Order.objects.get_or_create(
+                customer=customer,
+                complete=False
+            )
+            context = super(ProductList, self).get_context_data(**kwargs)
             context['cartItems'] = order.get_cart_items
         else:
-            context = super(ProductList,self).get_context_data(**kwargs)
+            context = super(ProductList, self).get_context_data(**kwargs)
             items = []
-            order = {'get_cart_total':0, 'get_cart_items':0}
+            order = {'get_cart_total': 0, 'get_cart_items': 0}
             cartItems = order['get_cart_items']
             context['cartItems'] = cartItems
-            
+
         return context
+
 
 def cart(request):
     '''
-    Fetches or creates a single :model: `store.Order` instance for the current customer dynamically
-    by logged in status and sets it's items before rendering it 
+    Fetches or creates a single :model: `store.Order` instance
+    for the current customer dynamically
+    by logged in status and sets it's items before rendering it
 
     **Context**
 
     ``items``
-        All items with this order ID to be attached to the order from :model: `OrderItem`
+        All items with this order ID to be attached
+        to the order from :model: `OrderItem`
 
     ``order``
-        The users current order/open cart object from :model: `store.Order`
+        The users current order/open cart
+        object from :model: `store.Order`
 
     ``cartItems``
-        Total number of cart items for the basket total from :model: `store.Order`
+        Total number of cart items for the
+        basket total from :model: `store.Order`
 
     :template:`store/cart.html`
     '''
 
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
         # Spoof for guest cart
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = order['get_cart_items']
 
     context = {
-        'items' : items,
-        'order' : order,
+        'items': items,
+        'order': order,
         'cartItems': cartItems
     }
     return render(request, 'store/cart.html', context)
+
 
 def product_detail(request, slug):
     """
@@ -105,7 +125,8 @@ def product_detail(request, slug):
         An instance of :model:`store.Product`.
 
     ``cartItems``
-        Total number of cart items for the basket total from :model:`store.OrderItem`
+        Total number of cart items for the
+        basket total from :model:`store.OrderItem`
 
     ``reviews``
         All reviews from :model:`store.Reviews` related to :model:`store.Order`
@@ -145,25 +166,30 @@ def product_detail(request, slug):
     # Create customer and cart
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
         # spoof for guest
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = order['get_cart_items']
 
     return render(
         request,
         "store/product_detail.html",
-        {"product": product,
-         "cartItems": cartItems,
-         "reviews": reviews,
-         "review_count": review_count,
-         "review_form": review_form,
+        {
+            "product": product,
+            "cartItems": cartItems,
+            "reviews": reviews,
+            "review_count": review_count,
+            "review_form": review_form,
         },
     )
+
 
 def checkout(request):
     """
@@ -175,10 +201,12 @@ def checkout(request):
         The customers current open instance of :model:`store.Order`
 
     ``items``
-        All items from :model:`OrderItems` related to thie :model:`store.Order` instance.
+        All items from :model:`OrderItems` related
+        to thie :model:`store.Order` instance.
 
     ``cartItems``
-        Total number of cart items for the basket total from :model:`store.OrderItem`
+        Total number of cart items for the basket
+        total from :model:`store.OrderItem`
 
     **Template:**
 
@@ -186,17 +214,20 @@ def checkout(request):
     """
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = order['get_cart_items']
 
     context = {
-        'items' : items,
-        'order' : order,
+        'items': items,
+        'order': order,
         'cartItems': cartItems
     }
     return render(request, 'store/checkout.html', context)
@@ -204,30 +235,37 @@ def checkout(request):
 
 def updateItem(request):
     """
-    Updates the instance of :model:`store.Order` for displaying the cart and checkout pages.
+    Updates the instance of :model:`store.Order`
+    for displaying the cart and checkout pages.
     """
 
     # Get the body sent in our fetch from updateUserOrder
     data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
-    
+
     # Get the customer
     customer = request.user.customer
     # Get the product
     product = Product.objects.get(id=productId)
     # Get or create the order
-    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    order, created = Order.objects.get_or_create(
+        customer=customer,
+        complete=False
+    )
     # Get or create the item
-    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    orderItem, created = OrderItem.objects.get_or_create(
+        order=order,
+        product=product
+    )
 
     # Update item quantity
     if action == 'add':
         orderItem.quantity = (orderItem.quantity + 1)
-            
+
     elif action == 'remove':
         orderItem.quantity = (orderItem.quantity - 1)
-    
+
     # Save the item
     orderItem.save()
 
@@ -238,26 +276,33 @@ def updateItem(request):
 
     return JsonResponse('Item was added', safe=False)
 
+
 def processOrder(request):
-    ''' 
+    '''
     Uses json and the fetch api to process the :model:`Order` for payment.
     Currently spoofed as out of scope for this assignment.
     '''
     transaction_id = datetime.datetime.now().timestamp()
-    data =json.loads(request.body)
+    data = json.loads(request.body)
 
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
-        # Check cart items haven't been manipulated in front end with JS before setting order to complete
-        # Below only works with guest cart implemented. Commented out to implement after marking.
+        # Check cart items haven't been manipulated in front end
+        # with JS before setting order to complete
+        # Below only works with guest cart implemented.
+        # Commented out to implement after marking.
         # if total == order.get_cart_total:
         #    order.complete = True
 
-        # Can be spoofed by hackers until I finish guest checkout but close order as completed for now
+        # Can be spoofed by hackers until I finish guest checkout
+        # but close order as completed for now
         order.complete = True
         order.save()
 
@@ -276,8 +321,9 @@ def processOrder(request):
 
     return JsonResponse('Payment Complete', safe=False)
 
+
 def review_edit(request, slug, review_id):
-    ''' 
+    '''
     View to edit :model:`store.Review` on given :model:`store.Prodct`.
     '''
 
@@ -295,9 +341,14 @@ def review_edit(request, slug, review_id):
             review.save()
             messages.add_message(request, messages.SUCCESS, 'Review Updated!')
         else:
-            messages.add_messages(request, messages.ERROR, 'Error updating review!')
+            messages.add_messages(
+                request,
+                messages.ERROR,
+                'Error updating review!'
+            )
 
         return HttpResponseRedirect(reverse('product_detail', args=[slug]))
+
 
 def review_delete(request, slug, review_id):
     """
@@ -311,6 +362,10 @@ def review_delete(request, slug, review_id):
         review.delete()
         messages.add_message(request, messages.SUCCESS, 'Review deleted!')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own reviews!')
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'You can only delete your own reviews!'
+        )
 
     return HttpResponseRedirect(reverse('product_detail', args=[slug]))
